1b04f1fb306999ccb705af27c3faea794c6186fd
GustavoLuzz <gustavo.barros@sptech.school>
2025-10-26 23:06:05 -0300

    :sparkles: feat: add innitial login page

---
+++ app/login/page.tsx
@@ -0,0 +1,34 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/components/login-form"
+import { ModeToggle } from "@/components/toggle-menu"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        {/* Linha do logo + toggle */}
+        <div className="flex items-center justify-between">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Spendwise
+          </a>
+          <ModeToggle />
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="https://images.pexels.com/photos/2008426/pexels-photo-2008426.jpeg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
